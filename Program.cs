eeb308f0103ca418dbaaa296fd2f1e378eddd2da
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-30 20:09:45 -0300

    Versão inicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿decimal valorCompra, valorPago, valorTroco;
+
+Console.WriteLine("--- Troco ---\n");
+
+Console.Write("Valor da compra (R$)...: ");
+valorCompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor pago (R$)........: ");
+valorPago = Convert.ToDecimal(Console.ReadLine());
+
+valorTroco = valorPago - valorCompra;
+
+Console.WriteLine($"\nTroco: {valorTroco:C}");
